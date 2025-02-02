457df987c6a0ec0f2f52de99107a8bfab7a9e13e
kengomasunari2025 <kengomasunari2025@gmail.com>
2025-02-02 16:16:43 +0900

    1回目のpush

---
+++ src/React/Kengo.tsx
@@ -0,0 +1,18 @@
+import SlickSlider from "./SlickSlider/SlickSlider";
+import Footer from "./Footer/Footer";
+// git clone https://github.com/kengomasunari2025/github-netlify-kengo.git
+// cd 20250128
+// git config --global user.name "kengomasunari2025"
+// git config --global user.email "kengomasunari2025@gmail.com"
+// git push origin main 
+// git pull origin main
+
+const Kengo = () => {
+  return (
+    <div className="bg-white">
+      <SlickSlider />
+      <Footer />
+    </div>
+  );
+};
+export default Kengo;
